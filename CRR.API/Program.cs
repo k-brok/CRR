e98052e87960cc33256d1715aaf104e668acb7b9
Kasper Brok <kasperbrok@gmail.com>
2025-07-10 13:44:59 +0200

    Enable swagger for production

--- CRR.API/Program.cs
+++ CRR.API/Program.cs
@@ -45,11 +45,11 @@ public class Program
         }
 
         // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
+        //if (app.Environment.IsDevelopment())
+        //{
             app.UseSwagger();
             app.UseSwaggerUI();
-        }
+        //}
 
         app.UseHttpsRedirection();
 
