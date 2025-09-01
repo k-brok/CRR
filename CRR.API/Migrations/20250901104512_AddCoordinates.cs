96a84b15f52006a1d567ae9b053e1aba8fad9924
Kasper Brok <kasperbrok@gmail.com>
2025-09-01 13:00:44 +0200

    Added lat and long

---
+++ CRR.API/Migrations/20250901104512_AddCoordinates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRR.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCoordinates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Addresses",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Addresses",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Addresses");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Addresses");
+        }
+    }
+}
--- CRR.Shared/Entities/Address.cs
+++ CRR.Shared/Entities/Address.cs
@@ -1,11 +1,15 @@
 using CRR.Shared.Enums;
 
 namespace CRR.Shared.Entities;
-public class Address{
-	public Guid Id {get; set;}
-	public string Name {get; set;} = string.Empty;
-	public string Street {get; set;}
-	public string Number {get; set;}
-	public string ZipCode {get; set;}
-	public TripType Type {get; set;}
+
+public class Address
+{
+	public Guid Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public string Street { get; set; }
+	public string Number { get; set; }
+	public string ZipCode { get; set; }
+	public TripType Type { get; set; }
+    public double Latitude { get; set; }
+	public double Longitude { get; set; }
 }
