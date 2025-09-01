293fb7d8ae7e0d53db467f52d079905ed7a4a907
Kasper Brok <kasperbrok@gmail.com>
2025-09-01 10:04:53 +0200

    Added tripTime

--- CRR.Shared/DTOs/DefaultTripDto.cs
+++ CRR.Shared/DTOs/DefaultTripDto.cs
@@ -4,17 +4,19 @@ public class DefaultTripDto
 {
     public Guid Id { get; set; }
     public string Type { get; set; } = "Business";
-    public Guid FromId {get; set;}
+    public Guid FromId { get; set; }
     public Guid ToId { get; set; }
     public int DefaultMileage { get; set; }
     public int PrivateMileage { get; set; }
+    public TimeSpan TripTime { get; set; }
 }
 
 public class CreateDefaultTripDto
 {
     public string Type { get; set; } = "Business";
-    public Guid FromId {get; set;}
+    public Guid FromId { get; set; }
     public Guid ToId { get; set; }
     public int DefaultMileage { get; set; }
     public int PrivateMileage { get; set; }
+    public TimeSpan TripTime { get; set; }
 }
