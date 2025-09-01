6e1303e1ee02a452a433eb607e1741911cbb3023
Kasper Brok <kasperbrok@gmail.com>
2025-09-01 08:42:42 +0200

    Added Triptime

--- CRR.Shared/Entities/DefaultTrip.cs
+++ CRR.Shared/Entities/DefaultTrip.cs
@@ -4,12 +4,13 @@ namespace CRR.Shared.Entities{
 	public class DefaultTrip
 	{
 		public TripType Type { get; set; }
-		public Guid Id {get; set;}
-		public Address From {get; set;}
-		public Guid FromId {get; set;}
-		public Address To {get; set;}
-		public Guid ToId {get; set;}
+		public Guid Id { get; set; }
+		public Address From { get; set; }
+		public Guid FromId { get; set; }
+		public Address To { get; set; }
+		public Guid ToId { get; set; }
 		public int DefaultMileage { get; set; }
 		public int PrivateMileage { get; set; }
+		public TimeSpan TripTime { get; set; }
 	}
 }
